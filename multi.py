f3b6d67ead164b4791df163c03abe7f4e9c873bc
ShakunIlya74 <maksdin11@gmail.com>
2021-12-19 16:40:58 +0300

    mult 2 process + time diff

--- multi.py
+++ multi.py
@@ -5,7 +5,11 @@ import itertools
 
 TIME_TO_SLEEP = 10
 
-
+def rest(flo, timer):
+	i = 0
+	while((flo - timer*i) > 0):
+		i+=1
+	return (timer*i - flo, i)
 
 def sleepy_man1():
     print('Man 1 is starting to sleep')
@@ -23,6 +27,8 @@ sleepy_people = [sleepy_man1, sleepy_man2]
  
 sleepy_people = itertools.cycle(sleepy_people)
 if __name__ == '__main__':
+    period = 0
+    time_diff = 0
     tic = time.time()
     for person in sleepy_people:
         local_tic = time.time()
@@ -32,7 +38,16 @@ if __name__ == '__main__':
         toc = time.time()
         time_spent = toc - local_tic
         print(f'Done in {time_spent:.4f} seconds')
-        if time_spent < TIME_TO_SLEEP:
-            time_to_wait = (toc - tic) % TIME_TO_SLEEP
-            print(f'Done early, wait {time_to_wait:.4f} seconds')
-            time.sleep((toc - tic) % TIME_TO_SLEEP)
+        sleep, i = rest(time_spent, TIME_TO_SLEEP)
+        period+=i
+        #print(period)
+        time.sleep(sleep - time_diff)
+        if (i == 1):
+            print(f'Done early, wait {sleep:.4f} seconds')
+        else:
+            print(f'Done later, wait {sleep:.4f} seconds')
+        
+        local_toc = time.time()
+        time_diff = local_toc - tic - TIME_TO_SLEEP * period
+        print(f'Accuracy of 10ss {time_diff:.4f} seconds')
+        
